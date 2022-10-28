4306162908aa634f0d6424090941feb5937aeb16
ILLIA H <i22holovkots@gmail.com>
2022-10-28 13:05:29 +0000

    Added advertising card list

---
+++ js/advertising.js
@@ -0,0 +1,41 @@
+(function(){
+
+    const advertising_products = [
+        {
+            id: "1",
+            title: "For Rabbits",
+            imageUrl: "img/rabbit_species.png",
+            description: "Rabbit",
+        },
+        {
+            id: "2",
+            title: "For Fish",
+            imageUrl: "img/fish_species.png",
+            description: "Fish",
+        },
+        {
+            id: "3",
+            title: "For Hamsters",
+            imageUrl: "img/hamster_species.png",
+            description: "Hamster",
+        },
+        {
+            id: "4",
+            title: "For Cats",
+            imageUrl: "img/cat_species.png",
+            description: "Cat",
+        },
+        {
+            id: "5",
+            title: "For Dogs",
+            imageUrl: "img/dog_species.png",
+            description: "Hamster",
+        },
+        {
+            id: "6",
+            title: "For Parrots",
+            imageUrl: "img/parrot_species.png",
+            description: "Hamster",
+        },
+    ]
+})();
