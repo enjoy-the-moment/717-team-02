9aa289c081a47fd42de4d5599fde516018854345
ILLIA H <i22holovkots@gmail.com>
2022-10-30 20:47:28 +0000

    Added a working form to subscribe using formspree (in the footer)

--- js/advertising-carousel.js
+++ js/advertising-carousel.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const slides = [
         '<div class = "advertising__slide"><img src="img/advertising/rabbit_species.png" alt="For Rabbits"></div>',
         '<div class = "advertising__slide"><img src="img/advertising/fish_species.png" alt="For Fish"></div>',
@@ -8,40 +8,40 @@
         '<div class = "advertising__slide"><img src="img/advertising/parrot_species.png" alt="For Parrots"></div>',
     ];
 
-    let currentSlideIdx = 0;
+let currentSlideIdx = 0;
 
-    function renderSlide() {
-        const slideContainer = document.querySelector('.species__carousel__slide-container');
-        slideContainer.innerHTML = slides[currentSlideIdx];
+function renderSlide() {
+    const slideContainer = document.querySelector('.species__carousel__slide-container');
+    slideContainer.innerHTML = slides[currentSlideIdx];
 
-        if (window.innerWidth > 780) {
-            const AddTwoSlideIndex = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-            slideContainer.innerHTML += slides[AddTwoSlideIndex];
-            if (window.innerWidth > 900) {
-                const AddThirdSlideIndex = AddTwoSlideIndex + 1 >= slides.length ? 0 : AddTwoSlideIndex + 1;
-                slideContainer.innerHTML += slides[AddThirdSlideIndex];
-            }
+    if (window.innerWidth > 780) {
+        const AddTwoSlideIndex = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+        slideContainer.innerHTML += slides[AddTwoSlideIndex];
+        if (window.innerWidth > 900) {
+            const AddThirdSlideIndex = AddTwoSlideIndex + 1 >= slides.length ? 0 : AddTwoSlideIndex + 1;
+            slideContainer.innerHTML += slides[AddThirdSlideIndex];
         }
     }
+}
 
-    function nextSlide() {
-        currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-        renderSlide();
-    }
-
-    function prevSlide() {
-        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
-        renderSlide();
-    }
-
-    // setInterval(nextSlide, 1000);
+function nextSlide() {
+    currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
     renderSlide();
+}
 
-    const nextButton = document.querySelector('.species__carousel-next');
-    nextButton.addEventListener('click', nextSlide)
+function prevSlide() {
+    currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
+    renderSlide();
+}
 
-    const prevButton = document.querySelector('.species__carousel-prev');
-    prevButton.addEventListener('click', prevSlide)
+// setInterval(nextSlide, 1000);
+renderSlide();
 
-    window.addEventListener('resize', renderSlide);
-})();
+const nextButton = document.querySelector('.species__carousel-next');
+nextButton.addEventListener('click', nextSlide)
+
+const prevButton = document.querySelector('.species__carousel-prev');
+prevButton.addEventListener('click', prevSlide)
+
+window.addEventListener('resize', renderSlide);
+}) ();
--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,47 @@
 
 (function() {
 
-    const fclockContainer = document.querySelector('.footer__clock');
-    fclockContainer.innerText = new Date().toLocaleTimeString();
-    function fupdateTime() {
-        fclockContainer.innerText = new Date().toLocaleTimeString();
-    }
+    // const fclockContainer = document.querySelector('.footer__clock');
+    // fclockContainer.innerText = new Date().toLocaleTimeString();
+    // function fupdateTime() {
+    //     fclockContainer.innerText = new Date().toLocaleTimeString();
+    // }
 
-    setInterval(fupdateTime, 1000);
+    // setInterval(fupdateTime, 1000);
+    const form = document.querySelector("#contacts_species");
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      const status = document.getElementById("contact-form-status");
+      fetch(event.target.action, {
+        method: form.method,
+        body: new FormData(event.target),
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+          } else {
+            response.json().then((data) => {
+              if (Object.hasOwn(data, "errors")) {
+                status.innerHTML = data["errors"]
+                  .map((error) => error["message"])
+                  .join(", ");
+              } else {
+                status.innerHTML =
+                  "Oops! There was a problem submitting your form";
+              }
+            });
+          }
+        })
+        .catch((error) => {
+          status.innerHTML = "Oops! There was a problem submitting your form";
+        });
+    }
+  
+    form.addEventListener("submit", handleSubmit);
 
 })();
