028f6cfffee9119a05de5ebbf06fae5582fa8d2b
ILLIA H <i22holovkots@gmail.com>
2022-10-30 14:14:00 +0000

    Changed classes and styles in the product list, added an array for the carousel (section advertising)

--- js/advertising-carousel.js
+++ js/advertising-carousel.js
@@ -1,5 +1,13 @@
 (function() {
 
+    const slides = [
+        '<div class = "advertising__slide"><img src="img/advertising/rabbit_species.png" alt="For Rabbits"></div>',
+        '<div class = "advertising__slide"><img src="img/advertising/fish_species.png" alt="For Fish"></div>',
+        '<div class = "advertising__slide"><img src="img/advertising/hamster_species.png" alt="For Hamsters"></div>',
+        '<div class = "advertising__slide"><img src="img/advertising/car_species.png" alt="For Cats"></div>',
+        '<div class = "advertising__slide"><img src="img/advertising/dog_species.png" alt="For Dogs"></div>',
+        '<div class = "advertising__slide"><img src="img/advertising/parrot_species.png" alt="For Parrots"></div>',
+    ];
+    let currentSlideIdx = 0;
 
-    
 })();
--- js/advertising-list.js
+++ js/advertising-list.js
@@ -47,17 +47,17 @@
 
     function renderProducts(products) {
         
-        const productsContainer = document.querySelector('.species__carousel');
+        const productsContainer = document.querySelector('.species__list');
         productsContainer.innerHTML = '';
 
         for (const product of products) {
             productsContainer.innerHTML += `
         <article class="species_carousel-item">
         <div class="species__image-container">
-            <img src="${product.imageUrl}" alt="Rabbit" class="species__carousel-item-image">
+            <img src="${product.imageUrl}" alt="Rabbit" class="species__list-item-image">
         </div>
-        <h3 class="species__carousel-item-title">${product.title}<span>→</span></h3>
-        <p class="species__carousel-item-explanation">${product.description}</p>
+        <h3 class="species__list-item-title">${product.title}<span>→</span></h3>
+        <p class="species__list-item-explanation">${product.description}</p>
     </article>`;
         }
     }
