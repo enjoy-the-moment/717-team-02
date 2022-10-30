488385c6f10ece02901010d203d7ebc37b74b17d
ILLIA H <i22holovkots@gmail.com>
2022-10-30 17:16:02 +0000

    Added active carousel buttons (advertising)

--- js/advertising-carousel.js
+++ js/advertising-carousel.js
@@ -21,7 +21,18 @@
         renderSlide();
     }
 
-    setInterval(nextSlide, 1000);
+    function prevSlide() {
+        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
+        renderSlide();
+    }
+
+    // setInterval(nextSlide, 1000);
     renderSlide();
+
+    const nextButton = document.querySelector('.species__carousel-next');
+    nextButton.addEventListener('click', nextSlide)
+
+    const prevButton = document.querySelector('.species__carousel-prev');
+    prevButton.addEventListener('click', prevSlide)
 
 })();
