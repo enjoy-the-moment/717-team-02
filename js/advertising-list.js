30eb1d060e69bec723da917f87fc7741e2bf6ebc
ILLIA H <i22holovkots@gmail.com>
2022-10-30 19:56:43 +0000

    Product list (advertising) converted to json, query made

--- js/advertising-list.js
+++ js/advertising-list.js
@@ -1,49 +1,7 @@
-(function () {
+(async function () {
 
-    const products = [
-        {
-            id: "1",
-            title: "For Rabbits",
-            imageUrl: "img/advertising/rabbit_species.png",
-            description: "Food • Treats • Hay • Cages • Litter & Bedding • Harnesses • Toys • Odor Removers ",
-
-        },
-        {
-            id: "2",
-            title: "For Fish",
-            imageUrl: "img/advertising/fish_species.png",
-            description: "Food • Feeders • Aquariums • Filters & Pumps • Cleaning • Water Care • Decor ",
-
-        },
-        {
-            id: "3",
-            title: "For Hamsters",
-            imageUrl: "img/advertising/hamster_species.png",
-            description: "Food • Soft & Chewy Treats • Litter • Furniture • Collars and Leashes • Bowls • Toys • Beds ",
-
-        },
-        {
-            id: "4",
-            title: "For Cats",
-            imageUrl: "img/advertising/cat_species.png",
-            description: "Dry Food • Wet Food • Treats • Catnip & Grass • Litter Boxes • Bowls & Feeders • Toys • Beds ",
-
-        },
-        {
-            id: "5",
-            title: "For Dogs",
-            imageUrl: "img/advertising/dog_species.png",
-            description: "Dry & Canned Food • Jerky • Chewy Treats • Bowls • Toys • Collars and Leashes • Clothes ",
-
-        },
-        {
-            id: "6",
-            title: "For Parrots",
-            imageUrl: "img/advertising/parrot_species.png",
-            description: "Food • Treats • Cages & Stands • Vitamins • Bowls & Feeders • Litter & Nesting • Odor Control ",
-
-        },
-    ];
+    const response = await fetch('api/advertising/products.json');
+    const products = await response.json();
 
     function renderProducts(products) {
         
