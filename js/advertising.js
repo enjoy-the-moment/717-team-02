126a84a05a8f002b83a1f8da2a97b9931d307cd4
ILLIA H <i22holovkots@gmail.com>
2022-10-30 12:54:14 +0000

    Removed the old list of products in advertising, created a new object with products, created a function to display

--- js/advertising.js
+++ js/advertising.js
@@ -4,24 +4,50 @@
         {
             id: "1",
             title: "For Rabbits",
-            imageUrl: "img/rabbit_species.png",
+            imageUrl: "img/advertising/rabbit_species.png",
             description: "Food • Treats • Hay • Cages • Litter & Bedding • Harnesses • Toys • Odor Removers ",
 
         },
         {
             id: "2",
             title: "For Fish",
-            imageUrl: "img/fish_species.png",
+            imageUrl: "img/advertising/fish_species.png",
             description: "Food • Feeders • Aquariums • Filters & Pumps • Cleaning • Water Care • Decor ",
 
         },
         {
             id: "3",
             title: "For Hamsters",
-            imageUrl: "img/rabbit_species.png",
+            imageUrl: "img/advertising/hamster_species.png",
             description: "Food • Soft & Chewy Treats • Litter • Furniture • Collars and Leashes • Bowls • Toys • Beds ",
 
         },
+        {
+            id: "4",
+            title: "For Cats",
+            imageUrl: "img/advertising/cat_species.png",
+            description: "Dry Food • Wet Food • Treats • Catnip & Grass • Litter Boxes • Bowls & Feeders • Toys • Beds ",
+
+        },
+        {
+            id: "5",
+            title: "For Dogs",
+            imageUrl: "img/advertising/dog_species.png",
+            description: "Dry & Canned Food • Jerky • Chewy Treats • Bowls • Toys • Collars and Leashes • Clothes ",
+
+        },
+        {
+            id: "6",
+            title: "For Parrots",
+            imageUrl: "img/advertising/parrot_species.png",
+            description: "Food • Treats • Cages & Stands • Vitamins • Bowls & Feeders • Litter & Nesting • Odor Control ",
+
+        },
     ]
+    function renderProducts(products) {
+        const productsContainer = document.querySelector('.species__carousel');
+        productsContainer.innerHTML = '';
+    }
+    
 
 })();
