6e5f920e961a208ab5cafe4a22ed0f1f2f6ffb5b
Ivan Kirillov <kirillov665@gmail.com>
2022-11-05 14:08:05 +0200

    change block Our New Arrivals

---
+++ js/arrivals.js
@@ -0,0 +1,7 @@
+(function () {
+    // const slide = document.querySelectorAll('.arrivals__carousel-card');
+    // const slides = Array.prototype.slice.call(slide);
+    // console.log(slides)
+
+
+})();
