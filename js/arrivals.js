5a4333311cff2492399c04c82cd66dc1d7139176
Ivan Kirillov <kirillov665@gmail.com>
2022-11-06 22:28:15 +0200

    fixed arrivals js

--- js/arrivals.js
+++ js/arrivals.js
@@ -6,6 +6,7 @@
         '<article class="arrivals__carousel-card"><a class="arrivals__carousel-card-link" href="#"><img src="../img/our_new_arrivals/feeder_with_food.png"alt="plastic bowl"></a><h4 class="arrivals__carousel-card-product"><a class="arrivals__carousel-card-product-link" href="#">OPTY Plastic Bowl</a></h4><P class="arrivals__carousel-card-product-price">$3,00</P><button class="arrivals__carousel-card-product-button">Add to Cart</button></article>',
         '<article class="arrivals__carousel-card"><a class="arrivals__carousel-card-link" href="#"><img src="../img/our_new_arrivals/toy_butterfly.png"alt="toy with butterfly for animals"></a><h4 class="arrivals__carousel-card-product"><a class="arrivals__carousel-card-product-link" href="#">EGO Butterfly Toy</a></h4><P class="arrivals__carousel-card-product-price">$8,00</P><button class="arrivals__carousel-card-product-button">Add to Cart</button></article>',
     ];
+
     let currentSlideIdx = 0;
     function renderSlide() {
         const slideContainer = document.querySelector('.arrivals__carousel');
